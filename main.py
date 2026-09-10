33099a9a5ebe59c2856904d10c5a830ab38d82b1
DU Guanjin <26110078@connect.polyu.hk>
2026-09-10 19:51:14 +0800

    Add time-based greeting

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello COMP5241")
+# 获取当前时间
+# 根据当前小时判断是上午、下午还是晚上
+# 打印问候语 "Good morning/afternoon/evening, DU Guanjin!"
+import datetime
+hour = datetime.datetime.now().hour
+if hour < 12:
+    print("Good morning, DU Guanjin!")
+elif hour < 18:
+    print("Good afternoon, DU Guanjin!")
+else:
+    print("Good evening, DU Guanjin!")
